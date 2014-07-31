2521193d73ac5f55916d6c2b1b8fd076f33cc989
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-07-31 10:27:51 -0700

    Submit event now emits restriction and stop info. Geocoder now limited to WA area. Address candidate zoom buttons now work. Modified layout

--- script/index.js
+++ script/index.js
@@ -1,57 +1,79 @@
 ﻿/*global require*/
-require(["esri/map", "esri/dijit/Geocoder", "route-ui"], function (Map, Geocoder, RouteUI) {
-	var map, routeUI, geocoder;
+require([
+	"esri/map", "esri/dijit/Geocoder", "route-ui", "esri/graphic", "esri/tasks/RouteTask", "esri/tasks/RouteParameters"
+], function (Map, Geocoder, RouteUI, Graphic, RouteTask, RouteParameters) {
+	var map, routeUI, geocoder, routeTask;
 	map = new Map("map", {
-		basemap: "gray"
+		basemap: "gray",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7
 	});
 	routeUI = new RouteUI();
+	routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/");
 	document.getElementById("toolsPane").appendChild(routeUI.form);
 
 	routeUI.on("route-params-submit", function (e) {
-		console.log(e);
+		console.log("route-parameters-submitted", e);
+	});
+	/** Shows an info window for the current feature and also centers the map at that location.
+	 * @param {Event} e
+	 * @param {Object} e.feature - A regular object that can be used to construct a Graphic;
+	 */
+	routeUI.on("stop-goto-link-click", function (e) {
+		var feature = e.feature;
+		feature = new Graphic(feature);
+		feature.setInfoTemplate({content: "${*}", title: "${name}"});
+		map.infoWindow.setFeatures([feature]);
+		map.infoWindow.show(feature.geometry);
+		map.centerAt(feature.geometry);
 	});
 
-	geocoder = new Geocoder({
-		map: map,
-		arcgisGeocoder: {
-			placeholder: "Add stop",
-			sourceCountry: "US"
-		},
-		autoComplete: true
-	}, "geocoder");
+	map.on("load", function () {
 
-	/**
-	 * @typedef GeocodeResult
-	 * @property {Extent} extent
-	 * @property {Graphic} feature
-	 * @property {string} name
-	 */
 
-	/**
-	 * @typedef SelectResult
-	 * @property {GeocodeResult} result
-	 */
+		geocoder = new Geocoder({
+			map: map,
+			arcgisGeocoder: {
+				placeholder: "Add stop",
+				sourceCountry: "US",
+				searchExtent: map.extent
+			},
+			autoComplete: true
+		}, "geocoder");
 
-	/**
-	 * @typedef FindResult
-	 * @property {GeocodeResult[]} results
-	 * @property {string} value
-	 */
+		/**
+		 * @typedef GeocodeResult
+		 * @property {Extent} extent
+		 * @property {Graphic} feature
+		 * @property {string} name
+		 */
 
-	/**
-	 * @param {(SelectResult|FindResult)} result
-	 */
-	function onResult(result) {
-		var results = result.results ? result.results.results : result.result ? [result.result] : null;
-		if (results) {
-			results.forEach(function (result) {
-				routeUI.addStop(result);
-			});
+		/**
+		 * @typedef SelectResult
+		 * @property {GeocodeResult} result
+		 */
+
+		/**
+		 * @typedef FindResult
+		 * @property {GeocodeResult[]} results
+		 * @property {string} value
+		 */
+
+		/**
+		 * @param {(SelectResult|FindResult)} result
+		 */
+		function onResult(result) {
+			var results = result.results ? result.results.results : result.result ? [result.result] : null;
+			if (results) {
+				results.forEach(function (result) {
+					routeUI.addStop(result);
+				});
+			}
 		}
-	}
 
-	geocoder.on("find-results", onResult);
-	geocoder.on("select", onResult);
+		geocoder.on("find-results", onResult);
+		geocoder.on("select", onResult);
+	});
 
 
 });
--- script/route-ui/main.js
+++ script/route-ui/main.js
@@ -7,8 +7,76 @@ define([
 	"dojo/text!./data/sync/Attribute Names.txt",
 	"dojo/text!./data/async/Restrictions.txt"
 ], function (Evented, declare, syncValues, asyncValues, syncDescriptions, asyncDescriptions) {
+	var usedIds = [], supportsCustomEventConstructor = true, geocodeResultDataAttributeName = "data-geocode-result";
 
-	var usedIds = [];
+	// These should be checked by default.
+	var defaultRestrictionAttributeNamesRe = /^(?:(?:Avoid Carpool Roads)|(?:Avoid Express Lanes)|(?:Avoid Gates)|(?:Avoid Private Roads)|(?:Avoid Unpaved Roads)|(?:Driving an Automobile)|(?:Roads Under Construction Prohibited)|(?:Through Traffic Prohibited))$/;
+
+	/**
+	 * A feature (a.k.a. graphic) object returned from the ArcGIS REST API. Contains `geometry` and `attributes` properties.
+	 * @external Feature
+	 * {@link http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/Feature_object/02r3000000n8000000/ ArcGIS REST API Feature}
+	 */
+
+	/**
+	 * A set of {external:Feature} objects.
+	 * @external Feature
+	 * {@link http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/featureSet/02r300000047000000/ ArcGIS REST API FeatureSet}
+	 */
+
+	/**
+	 * @external AttributeParameterValue
+	 * {@link https://developers.arcgis.com/javascript/jsapi/routeparameters-amd.html#attributeparametervalues RouteParameters#attributeParameterValues}
+	 */
+
+	/**
+	 * @typedef {Object} RestrictionInfo
+	 * @property {string[]} restrictionAttributes
+	 * @property {(external:FeatureSet|Array.<external:AttributeParameterValue>)} attributeParameterValues
+	 */
+
+
+	/**
+	 * @typedef {Object} CustomEventInit
+	 * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#CustomEventInit CustomEventInit}
+	 * @param {boolean} bubbles - A boolean indicating whether the event bubbles up through the DOM or not. (default: false)
+	 * @param {boolean} cancelable - A boolean indicating whether the event is cancelable. (default: false)
+	 * @param {*} detail - The data passed when initializing the event.
+	 */
+
+
+	/**
+	 * @param {string} eventType
+	 * @param {CustomEventInit} params
+	 */
+	function createCustomEvent(eventType, params) {
+		var customEvent;
+
+		params = params || {};
+
+		if (supportsCustomEventConstructor) {
+			// Try to create a native event. Not all browsers support this, however.
+			try {
+				customEvent = new CustomEvent(eventType, params);
+			} catch (e) {
+				console.error("failed to create custom event using CustomEvent constructor", e);
+				// Set a flag so we know not to attempt this again.
+				supportsCustomEventConstructor = false;
+			}
+		}
+
+		// Try to create the custom event using the alternate method.
+		if (!customEvent) {
+			try {
+				customEvent = document.createEvent("CustomEvent");
+				customEvent.initCustomEvent(eventType, params.bubbles, params.cancelable, params.detail);
+			} catch (e) {
+				console.error("failed to create custom event using alt. method.", e);
+			}
+		}
+
+		return customEvent;
+	}
 
 	// Add an Array.contains function if it does not already exist.
 	if (!Array.prototype.contains) {
@@ -106,9 +174,10 @@ define([
 	/**
 	 * @param {*} value
 	 * @param {string} propertyName
-	 * @returns {HTMLDivElement}
+	 * @param {string} restrictionName
+	 * @returns {HTMLDivElement} <div><label />(<select />|<input />)</div>
 	 */
-	function createParameterValueControlDiv(value, propertyName) {
+	function createParameterValueControlDiv(value, propertyName, restrictionName) {
 		var div, label, control;
 
 		div = document.createElement("div");
@@ -127,6 +196,8 @@ define([
 			control.value = value;
 		}
 		control.id = createId(propertyName);
+		control.setAttribute("data-restriction-name", restrictionName);
+		control.setAttribute("data-property-name", propertyName);
 		label.htmlFor = control.id;
 		control.classList.add("form-control");
 		div.appendChild(control);
@@ -146,16 +217,23 @@ define([
 		checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.id = createId(restrictionParameterName);
+		checkbox.setAttribute("data-restriction-name", restrictionParameterName);
+		checkbox.classList.add("default-restriction");
+		if (defaultRestrictionAttributeNamesRe.test(restrictionParameterName)) {
+			//checkbox.checked = true; // doesn't set default for form reset.
+			checkbox.setAttributeNode(document.createAttribute("checked"));
+
+		}
 		label.htmlFor = checkbox.id;
 		label.appendChild(document.createTextNode(restrictionParameterName));
 		div.appendChild(checkbox);
 		div.appendChild(label);
 		controlContainer = document.createElement("div");
-		controlContainer.classList.add("container");
+		controlContainer.setAttribute("class", "restriction-container well well-sm");
 		div.appendChild(controlContainer);
 		for (var propName in obj) {
 			if (obj.hasOwnProperty(propName)) {
-				innerDiv = createParameterValueControlDiv(obj[propName], propName);
+				innerDiv = createParameterValueControlDiv(obj[propName], propName, restrictionParameterName);
 				controlContainer.appendChild(innerDiv);
 			}
 		}
@@ -168,33 +246,79 @@ define([
 	 * @return {HTMLFormElement}
 	 */
 	function createFormFromObjectProperties(obj) {
-		var form, innerObj, propName, div, submitButton, resetButton, stopList;
+		var form, innerObj, div, submitButton, resetButton, stopList, btnGroup, innerBtnGroup;
+
+		function createPanel(headingText) {
+			var panel, panelHeading, panelBody;
+			panel = document.createElement("div");
+			panel.id = createId(headingText);
+			panel.setAttribute("class", "panel panel-default");
+
+			panelHeading = document.createElement("div");
+			panelHeading.setAttribute("class", "panel-heading");
+			panelHeading.textContent = headingText;
+			panel.appendChild(panelHeading);
+
+			panelBody = document.createElement("div");
+			panelBody.setAttribute("class", "panel-body");
+			panel.appendChild(panelBody);
+			return panel;
+		}
 
 		form = document.createElement("form");
 		form.setAttribute("role", "form");
 		form.action = "#";
 
+		// Create stop list
 		stopList = document.createElement("ol");
 		stopList.classList.add("stop-list");
-		form.appendChild(stopList);
+		stopList.classList.add("list-group");
+		(function () {
+			var panel = createPanel("Stops");
+			panel.querySelector(".panel-body").appendChild(stopList);
+			form.appendChild(panel);
+		}());
 
-		for (propName in obj) {
-			if (obj.hasOwnProperty(propName)) {
-				innerObj = obj[propName];
-				div = createRestrictionControlsDiv(innerObj, propName);
-				form.appendChild(div);
+		// Create restrictions panel
+		(function () {
+			var panel = createPanel("Restrictions");
+			var panelBody = panel.querySelector(".panel-body");
+
+			for (var propName in obj) {
+				if (obj.hasOwnProperty(propName)) {
+					innerObj = obj[propName];
+					div = createRestrictionControlsDiv(innerObj, propName);
+					panelBody.appendChild(div);
+				}
 			}
-		}
+			form.appendChild(panel);
+		}());
+
+		btnGroup = document.createElement("div");
+		btnGroup.setAttribute("class", "btn-group btn-group-justified");
+
+		innerBtnGroup = document.createElement("div");
+		innerBtnGroup.setAttribute("class", "btn-group");
+		btnGroup.appendChild(innerBtnGroup);
 
 		submitButton = document.createElement("button");
 		submitButton.type = "submit";
-		submitButton.innerText = "Submit";
-		form.appendChild(submitButton);
+		submitButton.textContent = "Submit";
+		submitButton.setAttribute("class", "btn btn-primary");
+		innerBtnGroup.appendChild(submitButton);
+
+		innerBtnGroup = document.createElement("div");
+		innerBtnGroup.setAttribute("class", "btn-group");
+		btnGroup.appendChild(innerBtnGroup);
 
 		resetButton = document.createElement("button");
 		resetButton.type = "reset";
-		resetButton.innerText = "Reset";
-		form.appendChild(resetButton);
+		resetButton.textContent = "Reset";
+		resetButton.setAttribute("class", "btn btn-danger");
+		innerBtnGroup.appendChild(resetButton);
+
+		form.appendChild(btnGroup);
+
 
 		return form;
 	}
@@ -266,41 +390,79 @@ define([
 	 * Called from a link that is the child of
 	 * a list item.
 	 * @param {Event} e
-	 * @param {HTMLAnchorElement} e.target
+	 * @param {HTMLAnchorElement} e.currentTarget
 	 */
 	function removeItemFromList(e) {
-		var link = e.target;
-		var listItem = link.parentElement;
+		var link = e.currentTarget;
+		var div = link.parentElement;
+		var listItem = div.parentElement;
 		var list = listItem.parentElement;
 		list.removeChild(listItem);
 
 		return false;
 	}
 
+	/**
+	 * Create a list item representing a geocode result.
+	 * @param {Object} stop
+	 * @returns {HTMLLIElement} - The <li> element with have a data-geocode-result attribute.
+	 */
 	function createStopListItem(stop) {
-		var li, removeLink, feature;
-		console.log("stop", stop);
+		var li, removeLink, feature, gotoLink, span, btnGroup;
 		if (stop.feature) {
 			if (stop.feature.toJson) {
 				feature = stop.feature.toJson();
 			} else {
 				feature = stop.feature;
 			}
+			feature.attributes.Name = stop.name;
 			li = document.createElement("li");
-			// TODO: place attribute value here instead of generic text.
-			li.textContent = stop.name || "STOP ";
+			li.classList.add("stop");
+			li.classList.add("list-group-item");
+
+			btnGroup = document.createElement("div");
+			btnGroup.setAttribute("class", "btn-group btn-group-xs badge");
+
+			li.appendChild(btnGroup);
+
+			li.appendChild(document.createTextNode(stop.name || "STOP"));
+
 			removeLink = document.createElement("a");
 			removeLink.href = "#";
-			removeLink.classList.add("remove-link");
-			removeLink.textContent = "remove";
-			li.appendChild(removeLink);
-			li.setAttribute("data-graphic", JSON.stringify(stop));
-			removeLink.onclick = removeItemFromList;
+			removeLink.setAttribute("class", "remove-link btn btn-danger");
+			//removeLink.classList.add("remove-link");
+			//removeLink.classList.add("text-danger");
+			span = document.createElement("span");
+			span.setAttribute("class", "glyphicon glyphicon-remove");
+			removeLink.appendChild(span);
+
+			btnGroup.appendChild(removeLink);
+
+			gotoLink = document.createElement("a");
+			gotoLink.href = "#";
+			gotoLink.setAttribute("class", "goto-link btn btn-default");
+			span = document.createElement("span");
+			span.setAttribute("class", "glyphicon glyphicon-screenshot");
+			gotoLink.appendChild(span);
+
+			btnGroup.appendChild(gotoLink);
+
+			li.setAttribute(geocodeResultDataAttributeName, JSON.stringify(feature));
+
+			//removeLink.addEventListener("click", removeItemFromList);
+			removeLink.onclick =removeItemFromList;
 		}
 		return li;
 	}
 
-	/*
+
+
+	/**
+	 * A user interface for the 
+	 * {@link http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/Route_service_with_synchronous_execution/02r300000036000000/ sync}
+	 * and
+	 * {@link http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/Route_service_with_asynchronous_execution/02r300000275000000/ async}
+	 * ArcGIS REST API route services.
 	 * @property {HTMLFormElement} form
 	 * @property {Object} properties
 	 * @property {Object.<string, RestrictionInfo>} descriptions
@@ -311,18 +473,113 @@ define([
 		descriptions: null,
 		stopList: null,
 		/**
-		 * @param {esri/Graphic} stop
+		 * Gets an array of stop features.
+		 * @returns {Array.<external:Feature>}
+		 */
+		getStopFeatures: function() {
+			var stopListItems, stops, li, feature;
+
+			stopListItems = this.stopList.querySelectorAll("li.stop");
+			stops = [];
+
+			for (var i = 0, l = stopListItems.length; i < l; i += 1) {
+				li = stopListItems[i];
+				feature = li.getAttribute(geocodeResultDataAttributeName);
+				feature = JSON.parse(feature);
+				stops.push(feature);
+			}
+
+			return stops;
+		},
+		/**
+		 * Gets a set of restriction features for the currently selected restrictions.
+		 * @returns {RestrictionInfo}
+		 */
+		getRestrictions: function (asFeatureSet) {
+			var checkedBoxes, controls, features = [], restrictionAttributes = [];
+
+			/**
+			 * Creates a feature from a control.
+			 * @param {(HTMLSelectElement|HTMLInputElement)} control
+			 * @returns {external:Feature}
+			 */
+			function controlToFeature(control) {
+				return {
+					attributes: {
+						AttributeName: control.getAttribute("data-restriction-name"),
+						ParameterName: control.getAttribute("data-property-name"),
+						ParameterValue: Number(control.value)
+					}
+				};
+			}
+
+			/**
+			 * Creates an restriction object.
+			 * @returns {external:AttributeParameterValue}
+			 */
+			function controlToObject(control) {
+				return {
+					attributeName: control.getAttribute("data-restriction-name"),
+					parameterName: control.getAttribute("data-property-name"),
+					parameterValue: Number(control.value)
+				};
+			}
+
+			// Get all of the checked checkboxes.
+			checkedBoxes = this.form.querySelectorAll("input[type='checkbox']:checked");
+
+			for (var cbi = 0, cbl = checkedBoxes.length; cbi < cbl; cbi += 1) {
+				restrictionAttributes.push(checkedBoxes[cbi].getAttribute("data-restriction-name"));
+				// Get all controls representing a restriction parameter.
+				controls = checkedBoxes[cbi].parentElement.querySelector(".restriction-container").querySelectorAll("[data-restriction-name]");
+				for (var controlI = 0, controlL = controls.length; controlI < controlL; controlI += 1) {
+					if (asFeatureSet) {
+						features.push(controlToFeature(controls[controlI]));
+					} else {
+						features.push(controlToObject(controls[controlI]));
+					}
+				}
+			}
+
+			return {
+				restrictionAttributes: restrictionAttributes,
+				attributeParameterValues: asFeatureSet ? { features: features } : features
+			};
+		},
+		/**
+		 * Adds a stop to the list of stops displayed in the UI.
+		 * @param {external:Feature} stop
 		 */
 		addStop: function (stop) {
-			var li;
+			var li, self = this, gotoLink;
+
+			/**
+			 * Emits an event that indicates one of the stops' "goto" links have been clicked.
+			 * @param {Event} e - The current target is an <a>. <li> > <div> > <a>.
+			 * @returns {boolean} Returns false so link is not followed when clicked if this is its event handler.
+			 */
+			function emitGotoEvent(e) {
+				var link = e.currentTarget;
+				var div = link.parentElement;
+				var listItem = div.parentElement;
+				var feature = listItem.getAttribute(geocodeResultDataAttributeName);
+				feature = JSON.parse(feature);
+				self.emit("stop-goto-link-click", { feature: feature });
+
+				return false;
+			}
+
 			if (!stop) {
 				throw new TypeError("No stop was provided.");
 			} else if (stop.feature) {
 				li = createStopListItem(stop);
 				this.stopList.appendChild(li);
+				gotoLink = li.querySelector(".goto-link");
+				gotoLink.onclick = emitGotoEvent;
 			}
 		},
 		/**
+		 * Creates a new instance of this class.
 		 * @param {boolean} async - Set to true for async routing service, false for sync.
 		 * @constructs
 		 */
@@ -330,12 +587,21 @@ define([
 			var self = this, form, properties, descriptions;
 
 			function onFormSubmit() {
-				////var event = new CustomEvent("route-params-submit", {
-				////	detail: "Submitted route parameters go here"
-				////});
-				////self.form.dispatchEvent(event);
+				var customEvent, detail;
 
-				self.emit("route-params-submit", { detail: "Submitted route parameters go here" });
+				detail = self.getRestrictions(false);
+				detail.stops = self.getStopFeatures();
+
+				customEvent = createCustomEvent("route-params-submit", {
+					detail: detail
+				});
+
+				if (customEvent) {
+					self.form.dispatchEvent(customEvent);
+				}
+
+				// TODO: Actually emit the parameters that the user submitted.
+				self.emit("route-params-submit", detail);
 
 				return false;
 			}
